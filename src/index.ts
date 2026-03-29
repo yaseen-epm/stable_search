5bf8ab1f4ef8275d7b18a2bde2b5e5874708632c
Parash Kumar <parash.kumar@cantire.com>
2026-03-29 14:03:02 +0530

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { createApp } from "./app";
 
-const port = Number(process.env.PORT || 3000);
+const port = Number(process.env.PORT || 3333);
 const app = createApp();
 
 app.listen(port, () => {
